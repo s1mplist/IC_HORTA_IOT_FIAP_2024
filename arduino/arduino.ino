7390c967dbc2cfe2b51e581fffe2208f41884e3d
s1mplist <gustavopinheiro.az@outlook.com>
2024-08-25 19:55:07 -0300

    Versão da banca

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,25 +1,51 @@
-int led = 9;         // the PWM pin the LED is attached to
-int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
+#define RELE 11
+#define BUTTON 10
+#define umidadeAnalogica A0 // Pino A0 para leitura analógica do sensor
+#define umidadeDigital 7 // Pino 7 para leitura digital do sensor
 
-// the setup routine runs once when you press reset:
+const int THRESHOLD_RAW = 512; // Valor limite bruto para a leitura analógica do sensor
+const int DELAY_TIME = 2000; // Tempo de atraso em milissegundos
+
+int valorAnalogicoBruto; // Armazena o valor bruto da leitura analógica
+int valorumidadeDigital; // Armazena a saída digital do sensor de umidade do solo
+
 void setup() {
-  // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  Serial.begin(9600); // Inicializa a comunicação serial
+  
+  pinMode(RELE, OUTPUT); // Define RELE como saída
+  pinMode(BUTTON, INPUT); // Define BUTTON como entrada
+  
+  pinMode(umidadeAnalogica, INPUT); // Define umidadeAnalogica como entrada
+  pinMode(umidadeDigital, INPUT); // Define umidadeDigital como entrada
+
+  digitalWrite(RELE, LOW); // Garante que o relé comece desligado
 }
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // set the brightness of pin 9:
-  analogWrite(led, brightness);
+void readSensors() {
+  valorAnalogicoBruto = analogRead(umidadeAnalogica); // Leitura bruta do sensor analógico
+  valorumidadeDigital = digitalRead(umidadeDigital); // Leitura digital do sensor
+}
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
+void displayStatus() {
+  Serial.print("Leitura analogica: ");
+  Serial.print(valorAnalogicoBruto); // Imprime o valor bruto da leitura analógica
+  Serial.print(" - Leitura Digital: ");
+  Serial.print(valorumidadeDigital);
+  Serial.print(" - Status: ");
+  Serial.println(valorumidadeDigital == 0 ? "Solo umido" : "Solo seco");
+}
 
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+void controlRelay() {
+  if(valorAnalogicoBruto < THRESHOLD_RAW) {
+    digitalWrite(RELE, HIGH); // Ativa o relé
+  } else {
+    digitalWrite(RELE, LOW); // Mantém o relé desligado
   }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
+}
+
+void loop() {
+  readSensors(); // Lê os valores dos sensores
+  displayStatus(); // Exibe o status no monitor serial
+  controlRelay(); // Controla o relé com base nas leituras dos sensores
+  delay(DELAY_TIME); // Atraso de 500ms
 }
